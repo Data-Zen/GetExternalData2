202d2b34a30d453f07ae975cb9ebca2025db3539
Paul Kats <paulkats@gmail.com>
2016-01-14 16:34:30 -0800

    aaa

--- runGetExternalDataProcess_BC.sh
+++ runGetExternalDataProcess_BC.sh
@@ -3,10 +3,7 @@ SHELL=/bin/bash
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 #exec > >(tee .runGetExternalDataProcess.log)
 
-# Without this, only stdout would be captured - i.e. your
-# log file would not contain any error messages.
-# SEE answer by Adam Spiers, which keeps STDERR a seperate stream -
-# I did not want to steal from him by simply adding his answer to mine.
+
 #exec 2>&1
 
 #echo "Hello World!"
